6d1a01c6da20cea34b85dfbb079bc88f1ea3445a
wangzg <976967399@qq.com>
2019-12-31 20:28:37 +0800

    第一次修改提交至本地仓库

--- src/com/atgui/com/T.java
+++ src/com/atgui/com/T.java
@@ -2,6 +2,6 @@ package com.atgui.com;
 
 public class T {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world===");
     }
 }
